dad9f94db99c721ddb000a0532c896c2a84f4d31
Valerie Lemuth <valerie.lemuth@uni-weimar.de>
2017-06-01 19:21:41 +0200

    Last changes -done✨

--- source/List.hpp
+++ source/List.hpp
@@ -50,29 +50,23 @@ struct ListIterator{
     } 
 
     Self& operator++(){
-        /*if (m_node->m_next == nullptr){
-            std::cout << "This is the last node";
-            //return 0;
-        }*/
         m_node = m_node->m_next;
         return *this;
     } 
 
     Self operator++(int){
-        /*if (m_node->m_next == nullptr){
-            std::cout << "This is the last node";
-            //return 0;
-        }*/
         ListIterator old(*this);
         m_node = m_node->m_next;
         return old;
         
     } 
 
+    //compares the pointers, wether they are pointing to the same Element 
     bool operator==(const Self& x) const{
         return m_node == x.m_node;
     } 
 
+    //or not
     bool operator!=(const Self& x) const{
         return m_node != x.m_node;
     } 
@@ -84,6 +78,13 @@ struct ListIterator{
             return ListIterator(nullptr);
     }
 
+    Self prev() const{
+        if(m_node)
+            return ListIterator(m_node->m_prev);
+        else
+            return ListIterator(nullptr);
+    }
+
     private:
         //The Node the iterator is pointing to
         ListNode <T>* m_node = nullptr;
@@ -91,29 +92,26 @@ struct ListIterator{
 
 template <typename T>
 struct ListConstIterator{
-
     friend class List <T>;
-
     public:
         // not implemented yet
-
     private:
         ListNode <T>* m_node = nullptr;
 };
 
-//Aufg. 4.7
+//Aufg. 4.7, operators == and !=
 
 template <typename T>
-bool operator ==(List <T> const & xs , List <T> const & ys){
+bool operator ==(List <T> const& xs , List <T> const& ys){
     auto a = xs.begin();
     auto b = ys.begin();
     bool c = true;
-    if(xs.size() != ys.size()){
+    if(xs.size() != ys.size()){         //if not the same size, can't be ==
         c = false;
     }
     else{
         while(a != nullptr){
-            if(*a != *b){
+            if(*a != *b){              //if one element not the same, not ==
                 c = false;
             }
             ++a;
@@ -124,16 +122,16 @@ bool operator ==(List <T> const & xs , List <T> const & ys){
 }
 
 template <typename T>
-bool operator !=(List <T> const & xs , List <T> const & ys){
+bool operator !=(List <T> const& xs , List <T> const& ys){
     auto a = xs.begin();
     auto b = ys.begin();
     bool c = false;
-    if(xs.size() != ys.size()){
+    if(xs.size() != ys.size()){       //if not the same size, can't be == has to be !=
         c = true;
     }
     else{
         while(a != nullptr){
-            if(*a != *b){
+            if(*a != *b){            //if one element not the same, not == has to be !=
                 c = true;
             }
             ++a;
@@ -160,53 +158,57 @@ class List{
         //Aufg. 4.2
 
         //Default Constructor
-
         List():
             m_size (0),
             m_first (nullptr),
             m_last (nullptr) {}
 
-        //Dstructor
+        
+        bool empty() const{
+            return m_size == 0;
+        }
+        
+        std::size_t size() const{
+            return m_size;
+        }
 
+        //Destructor
         ~List(){
             clear();
         }
 
-        //Copy Constructor
+        //Copy Constructor, creates new list out of already existing list
 
-        List(List<T> const& list):                //schon implementierte Liste
-            m_first(nullptr),
+        List(List<T> const& list):                //already implemented list
+            m_first(nullptr),                     //set *this on nullptr
             m_last(nullptr) {
-                auto b = list.begin();
+                auto b = list.begin();            //iterate through list
                 while(b != nullptr){
-                    push_back(*b);              //Nicht push_front
+                    push_back(*b);                //push all elements into *this
                     ++b;
                 }
             }
 
-        //Move Constructor
+        //Move Constructor, moves elements from one list to the other, "steals them"
 
-        List(List<T>&& list2):
-            m_size(list2.m_size),
+        List(List<T>&& list2):                    
+            m_size(list2.m_size),                 
             m_first(list2.m_first),
             m_last(list2.m_last) {
                 list2.m_first = nullptr;
                 list2.m_last = nullptr;
                 list2.m_size = 0;
             }
-        
 
 
-        //http://en.cppreference.com/w/cpp/container/list/empty
-        bool empty() const{
-            return m_size == 0;
-        }
-        //http://en.cppreference.com/w/cpp/container/list/size
-        std::size_t size() const{
-            return m_size;
+        //Aufg. 4.12, Zuweisungsoperator
+
+        List<T> operator= (List<T> list){
+            swap(list);
+            return *this;
         }
 
-        //Aufg. 4.3
+        //Aufg. 4.3, push_front and pop_front
 
         void push_front(T const& v){
             if(empty()){
@@ -234,21 +236,24 @@ class List{
             }
         }
 
-        //Aufg. 4.9
+        //Aufg. 4.9, insert at certain position
 
-        /*void insert(const iterator& x, T const& v){         //not done
-            if(x == begin()){
+        void insert(iterator p, T const& v){         
+            if(p == begin()){
                 push_front(v);
             }
-            else if(x == end()){
+            else if(p == bend()){
                 push_back(v);
             }
             else{
-
+                ListNode <T>* newNode = new ListNode<T>(v, p.prev().m_node, p.m_node);
+                p.m_node->m_prev = newNode;
+                newNode->m_prev->m_next = newNode;
+                ++ m_size;
             }
-        }*/
+        }
 
-        //Aufg. 4.3
+        //Aufg. 4.3, pop_front, pop_back, front, back
 
         void pop_front(){
             if(empty()){
@@ -282,28 +287,27 @@ class List{
             }
         }
 
-        T front(){
+        //gets value of the first element
+        T front(){                                         
             if(empty()){
                 std::cout << "The list is empty";
                 return 0;
-            }
-            ListNode <T> front = *m_first;
-            return front.m_value;                         //m_first->m_value     
+            }                       
+            return m_first->m_value;     
         }
 
+        //gets value of the last element
         T back(){
             if(empty()){
                 std::cout << "The list is empty";
                 return 0;
-            }
-            ListNode <T> back = *m_last;
-            return back.m_value;                        //m_last->m_value 
+            }                       
+            return m_last->m_value; 
         } 
 
+        //Aufg. 4.4, clear, deletes all elements 
+
         void clear(){
-            /*m_first = nullptr;
-            m_last = nullptr;
-            m_size = 0;*/
             while(!empty()){
                 pop_back();
             }
@@ -311,7 +315,8 @@ class List{
 
         //Aufg. 4.6
 
-        ListIterator <T> begin() const{
+        //iterator points to first element
+        ListIterator <T> begin() const{         
             if(empty()){
                 return ListIterator<T>();
             }
@@ -320,30 +325,42 @@ class List{
             }
         }
 
-        ListIterator <T> end() const{
+        //iterator points to last element
+        ListIterator <T> end() const{           
             if(empty()){
                 return ListIterator<T>();
             }
             else{
                 return ListIterator<T>(m_last);
             }
+        }
+
+        //iterator points to element behind last element
+        ListIterator <T> bend() const{        
+            return ListIterator<T>();
         } 
         
-        //Aufg. 4.10
+        //Aufg. 4.10, turns list around
 
         void reverse(){                
-            int s = m_size;
+            int s = m_size;             //remember the size
             auto a = begin();
-            while(a != nullptr){
+            while(a != nullptr){        //Add all the elements (reversed) at the front
                 push_front(*a);
                 ++a;
             }
-            for(int i = 1; i <= s; ++i){
+            for(int i = 1; i <= s; ++i){        //Delete the old ones at the back
                 pop_back();
             }
         }
 
+        //Aufg. 4.12, swaps the elements of two lists
 
+        void swap(List<T>& list){
+            std::swap(m_size, list.m_size);
+            std::swap(m_first, list.m_first);
+            std::swap(m_last, list.m_last);
+        }
         
 
 
@@ -353,12 +370,14 @@ class List{
         ListNode <T>* m_last = nullptr;
 };
 
+//Aufg. 4.10, free reverse function 
+
 template <typename T> 
 List<T> reverse(List<T> const& list){
     List<T> list2;
     auto a = list.begin();
     while(a != nullptr){
-        list2.push_front(*a);
+        list2.push_front(*a);           //push elements of list reversed into list2
         ++a;
     }
     return list2;
--- source/TestList.cpp
+++ source/TestList.cpp
@@ -3,7 +3,7 @@
 #include "List.hpp"
 #include "iostream"
 
-TEST_CASE ("List_default_constructor ", "[default]")
+TEST_CASE ("List default constructor ", "[Aufg. 4.2]")
 {
   List <int> list;
   List <int> list2;
@@ -14,7 +14,7 @@ TEST_CASE ("List_default_constructor ", "[default]")
   REQUIRE (list2.size() == 1);
 }
 
-TEST_CASE ("add an element with push_front ", "[modifiers]")
+TEST_CASE ("add an element with push_front ", "[Aufg. 4.3]")
 {
   List <int> list;
   list.push_front(42);
@@ -24,7 +24,7 @@ TEST_CASE ("add an element with push_front ", "[modifiers]")
   REQUIRE (list.size() == 2);
 }
 
-TEST_CASE ("add an element with push_back ", "[modifiers]")
+TEST_CASE ("add an element with push_back ", "[Aufg. 4.3]")
 {
   List <int> list;
   list.push_back(42);
@@ -33,7 +33,7 @@ TEST_CASE ("add an element with push_back ", "[modifiers]")
   REQUIRE(3 == list.back());
 }
 
-TEST_CASE ("delete an element with pop_front ", "[modifiers]")
+TEST_CASE ("delete an element with pop_front ", "[Aufg. 4.3]")
 {
   List <int> list;
   list.push_back(42);
@@ -43,7 +43,7 @@ TEST_CASE ("delete an element with pop_front ", "[modifiers]")
   REQUIRE(3 == list.front());
 }
 
-TEST_CASE ("delete an element with pop_back ", "[modifiers]")
+TEST_CASE ("delete an element with pop_back ", "[Aufg. 4.3]")
 {
   List <int> list;
   list.push_back(42);
@@ -57,7 +57,7 @@ TEST_CASE ("delete an element with pop_back ", "[modifiers]")
   REQUIRE(0 == list2.size());
 }
 
-TEST_CASE (" should be empty after clearing ", "[ modifiers ]")
+TEST_CASE (" should be empty after clearing ", "[Aufg. 4.4]")
 {
   List <int> list;
   list.push_front(1);
@@ -68,7 +68,7 @@ TEST_CASE (" should be empty after clearing ", "[ modifiers ]")
   REQUIRE(list.empty());
 }
 
-TEST_CASE (" should be an empty range after default construction ","[iterators]")
+TEST_CASE (" should be an empty range after default construction ","[Aufg. 4.6]")
 {
   List <int> list;
   auto b = list.begin();
@@ -76,46 +76,46 @@ TEST_CASE (" should be an empty range after default construction ","[iterators]"
   REQUIRE (b == e);
 }
 
-TEST_CASE (" provide acces to the first element with begin ", "[iterators]")
+TEST_CASE (" provide acces to the first element with begin ", "[Aufg. 4.6]")
 {
   List <int> list;
   list.push_front (42);
   REQUIRE (42 == *list.begin());
 }
 
-TEST_CASE (" test == ", "[iterators]")
+TEST_CASE (" test == ", "[Aufg. 4.6]")        
 {
   List <int> list ;
-  list.push_front (42);
+  list.push_front (42);     //only one element, begin and end still on the same element
   auto b = list.begin();
   auto e = list.end();
   REQUIRE (b == e);
 }
 
-TEST_CASE (" test != ", "[iterators]")
+TEST_CASE (" test != ", "[Aufg. 4.6]")
 {
   List <int> list;
+  list.push_front (42);   //two elements begin and end are on different elements
   list.push_front (42);
-  list.push_front (43);
   auto b = list.begin();
   auto e = list.end();
   REQUIRE (b != e);
 }
 
-TEST_CASE ("default_constructor ", "[iterators]")
+TEST_CASE ("default constructor ", "[Aufg. 4.6]")
 {
   ListIterator<int> it;
   REQUIRE (it.next() == nullptr);
 }
 
-TEST_CASE ("other_constructor ", "[iterators]")
+TEST_CASE ("other constructor ", "[Aufg. 4.6]")
 {
   ListNode<int> n;
   ListIterator<int> it2(&n);
   REQUIRE (it2.next() == nullptr);
 }
 
-TEST_CASE ("operators ", "[iterators]")
+TEST_CASE ("pre- and postincrement  ", "[Aufg. 4.6]")
 {
   List <int> list;
   list.push_front(8);
@@ -129,13 +129,13 @@ TEST_CASE ("operators ", "[iterators]")
   REQUIRE (*(++b) == 5);          // 6>5 9 8  wird erhöht und neuer Knoten wird ausgegeben
   REQUIRE (*(b++) == 5);          // 6 5>9 8  wird erhöht und alter Wert wird ausgegeben
   REQUIRE (*(++b) == 8);          // 6 5 9>8  wird erhöht und neuer Knoten wird ausgegeben
-  REQUIRE ((++b) == nullptr);  // 6 5 9 8> vrgl. mit nullptr geht nicht obwohl es drauf zeigt
-  REQUIRE (*e == 8);              // 6 5 9 8<
+  REQUIRE ((++b) == nullptr);     // 6 5 9 8> wird erhöht und nullptr wird ausgegeben
+  REQUIRE (*e == 8);              // 6 5 9 8< letzter Wert
   REQUIRE (*(e++) == 8);          // 6 5 9 8  < wird erhöht und alter Wert wird ausgegeben 
-  REQUIRE (e == nullptr);      // 6 5 9 8  < vrgl. mit nullptr geht nicht obwohl es drauf zeigt
+  REQUIRE (e == nullptr);         // 6 5 9 8  < nullptr wird ausgegeben
 }
 
-TEST_CASE (" copy constructor ", "[ constructor ]")
+TEST_CASE (" copy constructor and comparing lists ", "[Aufg. 4.8]")
 {
   List <int> list;
   list.push_front(1);
@@ -146,68 +146,162 @@ TEST_CASE (" copy constructor ", "[ constructor ]")
   REQUIRE(list == list2);
   List <int> list3;
   List <int> list4;
-  REQUIRE(list != list3);         //list mit Elementen, list3 ohne
-  REQUIRE(list3 == list4);        //list3 ohne Elemente, list4 auch 
+  REQUIRE(list != list3);         
+  REQUIRE(list3 == list4);        
   list3.push_front(8);
   REQUIRE(list != list3);
 }
 
-TEST_CASE (" move constructor ", "[ constructor ]")
+TEST_CASE ("add an element with insert ", "[Aufg. 4.9]")
 {
   List <int> list;
-  list.push_front(1);
-  list.push_front(2);
-  list.push_front(3);
   list.push_front(4);
-  List <int> list2 (std::move(list));
-  REQUIRE(0 == list.size());
-  REQUIRE(list.empty());
-  REQUIRE(4 == list2.size());
+  list.push_front(2);
+  list.push_front(6);
+  
+  auto e = list.begin();      //list now: 624
+  REQUIRE (*e == 6);  
+  REQUIRE (*(++e) == 2);
+  REQUIRE (*(++e) == 4);
+  REQUIRE (list.size() == 3);
+
+  list.insert(list.begin(),9);        //Add at the front  
+
+  auto h = list.begin();      //list now: 9624
+  REQUIRE (*h == 9);  
+  REQUIRE (*(++h) == 6);
+  REQUIRE (*(++h) == 2);
+  REQUIRE (*(++h) == 4);
+  REQUIRE (list.size() == 4);
+
+  list.insert(list.end(),1);           //Add at the place of the last node
+
+  auto g = list.begin();      //list now: 96214
+  REQUIRE (*g == 9);  
+  REQUIRE (*(++g) == 6);
+  REQUIRE (*(++g) == 2);
+  REQUIRE (*(++g) == 1);      //not 96241 because end() is the last node not behind it
+  REQUIRE (*(++g) == 4);
+  REQUIRE (list.size() == 5);
+
+  list.insert(list.bend(),3);           //Add behind the last node
+
+  auto j = list.begin();      //list now: 962143
+  REQUIRE (*j == 9);  
+  REQUIRE (*(++j) == 6);
+  REQUIRE (*(++j) == 2);
+  REQUIRE (*(++j) == 1);
+  REQUIRE (*(++j) == 4);
+  REQUIRE (*(++j) == 3);
+  REQUIRE (list.size() == 6);
+
+  auto i = list.begin();
+  ++i;
+  ++i;    //i = 3
+
+  list.insert(i,7);
+
+  auto k = list.begin();      //list now: 9672143
+  REQUIRE (*k == 9);  
+  REQUIRE (*(++k) == 6);
+  REQUIRE (*(++k) == 7);
+  REQUIRE (*(++k) == 2);
+  REQUIRE (*(++k) == 1);
+  REQUIRE (*(++k) == 4);
+  REQUIRE (*(++k) == 3);
+  REQUIRE (list.size() == 7);
 }
 
-TEST_CASE ("reverse ", "[reverse]")
+TEST_CASE ("reverse ", "[Aufg. 4.10]")
 {
   List <int> list;
   list.push_front(8);
   list.push_front(9);
   list.push_front(5);
   list.push_front(6);
+
   auto b = list.begin();
 
-  REQUIRE (*b == 6);  
+  REQUIRE (*b == 6);            //list now: 6598
   REQUIRE (*(++b) == 5);
   REQUIRE (*(++b) == 9);
   REQUIRE (*(++b) == 8);
 
-  //std::cout<< "The front: " << list.front() << "\n";
-  //std::cout<< "The back: " << list.back() << "\n";
-
   list.reverse();
+
   auto e = list.begin();
 
-  //std::cout<< "The front2: " << list.front() << "\n";
-  //std::cout<< "The back2: " << list.back() << "\n";
 
-  REQUIRE (*e == 8);  
+  REQUIRE (*e == 8);            //list now: 8956
   REQUIRE (*(++e) == 9);
   REQUIRE (*(++e) == 5);
   REQUIRE (*(++e) == 6);
 }
 
-TEST_CASE ("free reverse ", "[reverse]")
+TEST_CASE ("free reverse ", "[Aufg. 4.10]")
 {
   List <int> list;
-  list.push_front(8);              //6598
+  list.push_front(8);              //list now: 6598
   list.push_front(9);
   list.push_front(5);
   list.push_front(6);
 
   auto r = reverse(list);
 
-  REQUIRE (*r.begin() == 8);      //8956
+  REQUIRE (*r.begin() == 8);      //list now: 8956
   REQUIRE (*r.begin().next() == 9);
   REQUIRE (*r.begin().next().next() == 5);
   REQUIRE (*r.end() == 6);
+}
+
+TEST_CASE ("List in Vector ", "[Aufg. 4.11]")
+{
+  List <int> list;
+  list.push_front(8);              //list now: 6598
+  list.push_front(9);
+  list.push_front(5);
+  list.push_front(6);
+
+  std::vector<int> v (list.size());
+
+  std::copy(list.begin(), list.bend(), std::begin(v));
+
+  REQUIRE (v[0] == 6);   
+  REQUIRE (v[1] == 5);
+  REQUIRE (v[2] == 9);   
+  REQUIRE (v[3] == 8);
+}
+
+TEST_CASE (" = operator (Zuweisung) ", "[Aufg. 4.12]")
+{
+  List <int> list;
+  list.push_front(1);
+  list.push_front(2);
+  list.push_front(3);
+  list.push_front(4);
+
+  List <int> list2;
+
+  REQUIRE(list2 != list);
+
+  list2 = list;
+
+  REQUIRE(list2 == list);
+  REQUIRE(list2.size() == list.size());
+}
+
+TEST_CASE (" move constructor ", "[Aufg. 4.13]")
+{
+  List <int> list;
+  list.push_front(1);
+  list.push_front(2);
+  list.push_front(3);
+  list.push_front(4);
+
+  List <int> list2 (std::move(list));
+  REQUIRE(0 == list.size());
+  REQUIRE(list.empty());
+  REQUIRE(4 == list2.size());
 }
 
 int main(int argc, char* argv[]){
